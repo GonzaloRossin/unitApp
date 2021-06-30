949fee607d187cec9477c101178470420c7da653
gonzaloRossin <gonzarossin@gmail.com>
2021-06-30 15:45:48 -0300

    map camera sets automatically on top of current location

--- app/src/main/java/com/example/unitapp/fragments/HomeFragment.java
+++ app/src/main/java/com/example/unitapp/fragments/HomeFragment.java
@@ -2,21 +2,28 @@ package com.example.unitapp.fragments;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.unitapp.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+import org.jetbrains.annotations.NotNull;
 
 import static com.example.unitapp.utils.Constants.MAPVIEW_BUNDLE_KEY;
 
@@ -24,21 +31,23 @@ import static com.example.unitapp.utils.Constants.MAPVIEW_BUNDLE_KEY;
 public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
     private MapView mMapView;
+    FusedLocationProviderClient fusedLocationClient;
     Button confirmButton;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.requireContext());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         mMapView = view.findViewById(R.id.mapView2);
-        confirmButton= view.findViewById(R.id.button3);
+        confirmButton = view.findViewById(R.id.button3);
         confirmButton.setOnClickListener(v -> {
-            ChooseRideFragment chooseRideFragment=new ChooseRideFragment();
+            ChooseRideFragment chooseRideFragment = new ChooseRideFragment();
             FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction().setReorderingAllowed(true);
             transaction.replace(R.id.container, chooseRideFragment);
             transaction.addToBackStack(null);
@@ -47,10 +56,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         initGoogleMap(savedInstanceState);
         return view;
     }
-
-    private void initGoogleMap(Bundle savedInstanceState){
+    private void initGoogleMap(Bundle savedInstanceState) {
         Bundle mapViewBundle = null;
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
 
@@ -58,14 +66,15 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
         mMapView.getMapAsync(this);
     }
+
     @Override
-    public void onSaveInstanceState(Bundle outState){
+    public void onSaveInstanceState(@NotNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
         Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
-        if(mapViewBundle == null){
+        if (mapViewBundle == null) {
             mapViewBundle = new Bundle();
-            outState.putBundle(MAPVIEW_BUNDLE_KEY,mapViewBundle);
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
         }
         mMapView.onSaveInstanceState(mapViewBundle);
     }
@@ -89,7 +98,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
+    public void onMapReady(@NotNull GoogleMap map) {
         if (ActivityCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -103,6 +112,12 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this.requireActivity(), location -> {
+            if(location!=null){
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()), 17);
+                map.animateCamera(cameraUpdate);
+            }
+        });
         map.setMyLocationEnabled(true);
     }
 
