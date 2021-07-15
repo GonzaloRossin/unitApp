f7de3390a05247dd5226a665edd5b4a98420c1d1
Matias Enrique Pavan <matiaspavan@gmail.com>
2021-07-14 22:54:01 -0300

    upgrade plan button on profile

--- app/src/main/java/com/example/unitapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/unitapp/fragments/ProfileFragment.java
@@ -5,19 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import com.example.unitapp.R;
 import com.example.unitapp.UnitApp;
 import com.example.unitapp.activities.LoadingActivity;
-import com.example.unitapp.activities.MainActivity;
 import com.example.unitapp.databinding.FragmentProfileBinding;
-import com.example.unitapp.databinding.FragmentRegisterBinding;
 import com.example.unitapp.repository.Status;
 
 import org.jetbrains.annotations.NotNull;
@@ -27,17 +21,20 @@ import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
 
+
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         FragmentProfileBinding binding = FragmentProfileBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-        UnitApp app = ((UnitApp)requireActivity().getApplication());
+        UnitApp app = ((UnitApp) requireActivity().getApplication());
+        
         binding.logOut.setOnClickListener(v -> {
             app.getPreferences().setCabifyToken(-1);
             app.getPreferences().setUberToken(-1);
@@ -46,15 +43,14 @@ public class ProfileFragment extends Fragment {
             startActivity(intent);
         });
         app.getUserRepository().getCurrentUser().observe(getViewLifecycleOwner(), r -> {
-            if(r.getStatus() == Status.SUCCESS) {
+            if (r.getStatus() == Status.SUCCESS) {
                 binding.profilePhone.setText(String.valueOf(Objects.requireNonNull(r.getData()).getPhone()));
                 binding.profileEmail.setText(r.getData().getEmail());
-                binding.cabify.setText(new StringBuilder().append(getString(R.string.link)).append(" Cabify").toString());
-                binding.uber.setText(new StringBuilder().append(getString(R.string.link)).append(" Uber").toString());
             }
         });
 
 
         return view;
     }
+
 }
